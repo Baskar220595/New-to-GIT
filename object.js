bde60fcbd4296303d71abc3149250dcefdb0c5da
Baskar220595 <baskar.ramu.rs@gmail.com>
2025-10-24 16:56:54 +0530

    Uptodate

---
+++ object.js
@@ -0,0 +1,16 @@
+let obj= {
+    name : "Baskar",
+    role: "Software Test Engineer"
+}
+let details= [
+    {  
+        id: "01",
+        role: "Software"},
+    {
+        id: "02",
+        role: "Test"},
+    {
+        id: "03",
+        role: "Engineer"
+    }]
+    console.log(details[1].id)
