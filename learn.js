e2bc3d70c06e4a50a564f5685176e0fb60024aa1
Baskar220595 <baskar.ramu.rs@gmail.com>
2026-02-04 16:18:29 +0530

    Uptodate

---
+++ learn.js
@@ -0,0 +1,21 @@
+function fun (){
+    
+var a = 10;
+var a = 20;
+console.log(a);
+
+let b = 10;
+ b = 20;
+console.log(b);
+
+const c = 500;
+const v = 20;
+console.log(c);
+}
+fun();
+
+
+function fun (a,b,c){
+    console.log(a+b+c);
+}
+fun(1,2,3)
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+function fun (){
+    
+var a = 10;
+var a = 20;
+console.log(a);
+
+let b = 10;
+ b = 20;
+console.log(b);
+
+const c = 500;
+const v = 20;
+console.log(c);
+}
+fun();
